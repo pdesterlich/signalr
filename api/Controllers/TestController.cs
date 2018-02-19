ab42241c96e7df165400293c417638ebc98b290a
Phelipe de Sterlich <p.desterlich@gmail.com>
2018-02-19 17:33:27 +0100

    aggiunto controller per invio messaggio a signalr da api

---
+++ api/Controllers/TestController.cs
@@ -0,0 +1,37 @@
+﻿using System;
+using System.Threading.Tasks;
+using Microsoft.AspNetCore.Mvc;
+using Microsoft.AspNetCore.SignalR.Client;
+
+namespace api.Controllers
+{
+    [Route("api/test")]
+    public class TestController: Controller
+    {
+        [HttpPost]
+        public async Task<IActionResult> Post([FromBody] MessageViewModel model)
+        {
+            Console.WriteLine($"ricevuto: {model.Messaggio}");
+            var connection = new HubConnectionBuilder()
+                .WithUrl("http://localhost:5001/chat")
+                .WithConsoleLogger()
+                .Build();
+
+            connection.On<string>("Send", data =>
+            {
+
+            });
+            
+            await connection.StartAsync();
+
+            await connection.InvokeAsync("Send", model.Messaggio);
+
+            return Ok();
+        }
+    }
+
+    public class MessageViewModel
+    {
+        public string Messaggio { get; set; }
+    }
+}
