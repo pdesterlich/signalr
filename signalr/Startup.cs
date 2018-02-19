48a516e0b902e0a28063615efa4a3343485167ac
Phelipe de Sterlich <p.desterlich@gmail.com>
2018-02-19 14:12:22 +0100

    aggiunte librerie signalr

--- signalr/Startup.cs
+++ signalr/Startup.cs
@@ -1,13 +1,8 @@
-﻿using System;
-using System.Collections.Generic;
-using System.Linq;
-using System.Threading.Tasks;
-using Microsoft.AspNetCore.Builder;
+﻿using Microsoft.AspNetCore.Builder;
 using Microsoft.AspNetCore.Hosting;
 using Microsoft.Extensions.Configuration;
 using Microsoft.Extensions.DependencyInjection;
-using Microsoft.Extensions.Logging;
-using Microsoft.Extensions.Options;
+using signalr.Hubs;
 
 namespace signalr
 {
@@ -23,6 +18,7 @@ namespace signalr
         // This method gets called by the runtime. Use this method to add services to the container.
         public void ConfigureServices(IServiceCollection services)
         {
+            services.AddSignalR();
             services.AddMvc();
         }
 
@@ -34,6 +30,8 @@ namespace signalr
                 app.UseDeveloperExceptionPage();
             }
 
+            app.UseSignalR(routes => { routes.MapHub<ChatHub>("chat"); });
+            
             app.UseMvc();
         }
     }
